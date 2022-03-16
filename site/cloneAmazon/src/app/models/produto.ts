bbc63fe169119d22d94839ae694887fa7f6b3b02
Luiz Henrique Santos <96305078+LuizHenriqueSantos15@users.noreply.github.com>
2022-03-16 16:16:22 -0300

    ajustando css pagina de compra

---
+++ site/cloneAmazon/src/app/models/produto.ts
@@ -0,0 +1,11 @@
+export class Produto {
+    nome:string;
+    preco:string;
+    juros:boolean;
+    frete:string;
+    resumo:string;
+    nicho:string;
+    subnicho:string;
+    foto:string;
+    
+}
